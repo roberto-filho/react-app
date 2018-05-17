7d2b2987e76c30320dc490e7371d7f60ab21f0d2
Roberto Filho <roberto.w.filho@gmail.com>
2018-05-16 23:06:25 -0300

    Upload file working.

---
+++ src/bank/SubmitFileForm.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import {post} from 'axios';
+import NotificationSystem from 'react-notification-system';
+
+export default class ProductForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      file: null
+    }
+  }
+
+  componentDidMount() {
+    this._notifications = this.refs.notifications;
+  }
+
+  notifyError(message) {
+    this._notifications.addNotification({
+      message: message,
+      level: 'error'
+    });
+  }
+
+  notifyInfo(message) {
+    this._notifications.addNotification({
+      message: message,
+      level: 'success'
+    });
+  }
+
+  getErrorFromResponse(error) {
+    const isValidationError = error.response && error.response.status === 422;
+    const isError = error.response && error.response.data;
+    let errorMessage = isValidationError
+      ? JSON.stringify(error.response.data)
+      : (isError ? error.response.data : error.toString());
+
+    return errorMessage;
+  }
+  
+  handleFileChange = (event) => {
+    console.log(event.target.files);
+    
+    this.setState({file: event.target.files[0]})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const url = 'http://localhost:9393/api/bank/upload';
+
+    const formData = new FormData();
+    formData.append('file', this.state.file)
+
+    post(url, formData, { headers: {'content-type': 'multipart/form-data'} })
+      .then(res => {
+        // Show success message
+        // Clear form fields
+        const successMessage = `Successfully uploaded file: [${this.file}]`;
+
+        this.notifyInfo(successMessage);
+
+      }).catch(error => {
+        // Show error message
+        this.notifyError(this.getErrorFromResponse(error));
+      });
+  }
+  
+  render() {
+    return <div>
+      <NotificationSystem ref= 'notifications' />
+      <form onSubmit={this.handleSubmit}>
+        <RaisedButton
+          containerElement='label'
+          label='Arquivo de transações'>
+          <input type='file' onChange={this.handleFileChange}/>
+        </RaisedButton>
+        <br />
+        <br />
+        <RaisedButton 
+          type={'submit'}
+          primary={true} 
+          label={'Save'} />
+      </form>
+    </div>
+  }
+};
--- src/layout/Body.jsx
+++ src/layout/Body.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Products from '../products/Products';
 import ProductForm from '../products/ProductForm';
+import BankSubmit from '../bank/SubmitFileForm';
+import Transactions from "../bank/Transactions";
 
 export default class Body extends React.Component {
   render() {
     return <main>
       <Switch>
-        {/* <Route exact path='/' component={Home}/> */}
-        <Route path='/products/new' component={ProductForm}/>
-        <Route path='/products/:id' component={ProductForm}/>
-        <Route path='/products' component={Products}/>
+        <Route exact path='/' component={BankSubmit} />
+        <Route path='/products/new' component={ProductForm} />
+        <Route path='/products/:id' component={ProductForm} />
+        <Route path='/products' component={Products} />
+        <Route path='/transactions' component={Transactions} />
       </Switch>
     </main>
   }
